45779830e25db6e6f9ad183d3d24736444cd532d
Fabio Dias <fbd581@gadi-login-06.gadi.nci.org.au>
2023-01-26 14:19:30 +1100

    add gadi scripts

--- Grid_avg_evolution/run_OHC+Salt+MKE_evolution.bash
+++ Grid_avg_evolution/run_OHC+Salt+MKE_evolution.bash
@@ -6,13 +6,17 @@
 #PBS -l walltime=1:00:00
 #PBS -l software=python
 #PBS -l wd
-#PBS -l storage=gdata/hh5
+#PBS -l storage=gdata/hh5+scratch/gh9
 ###PBS -l storage=scratch/gh9
 
  
 # Run Python applications
 #python3 /g/data3/hh5/tmp/access-om/fbd581/ROMS/postprocessing/waom_notebook/Grid_avg_evolution/WAOM10x4km_shflim_S_ORAS5em_0.25Q_OHC+Salt+MKE_evolution.py > $PBS_JOBID.log
 
-python3 /g/data3/hh5/tmp/access-om/fbd581/ROMS/postprocessing/waom_notebook/Grid_avg_evolution/WAOM10x4km_shflim_S_ORAS5emXECCO2_0.25Q_OHC+Salt+MKE_evolution.py > $PBS_JOBID.log
+#python3 /g/data3/hh5/tmp/access-om/fbd581/ROMS/postprocessing/waom_notebook/Grid_avg_evolution/WAOM10x4km_shflim_S_ORAS5emXECCO2_0.25Q_OHC+Salt+MKE_evolution.py > $PBS_JOBID.log
 
+#python3 /g/data3/hh5/tmp/access-om/fbd581/ROMS/postprocessing/waom_notebook/Grid_avg_evolution/WAOM10extend_shflim_S_0.25Q_OHC+Salt+MKE_evolution.py > $PBS_JOBID.log
 
+#python3 /g/data3/hh5/tmp/access-om/fbd581/ROMS/postprocessing/waom_notebook/Grid_avg_evolution/WAOM10extend_x4km_shflim_S_ORAS5emXECCO2_0.25Q_OHC+Salt+MKE_evolution.py > $PBS_JOBID.log
+
+python3 /g/data3/hh5/tmp/access-om/fbd581/ROMS/postprocessing/waom_notebook/Grid_avg_evolution/WAOM10extendXshrink_shflim_S_ORAS5emXECCO2_0.25Q_OHC+Salt+MKE_evolution.py > $PBS_JOBID.log
